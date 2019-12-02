a9641b5c2f1a443ca8b50694fb62673cead000ac
Takuto Koyama <koyama.takuto@kmw.jp>
2019-12-02 14:40:17 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import * as ramda from 'ramda'
+
 export function div(x: number, y: number) {
   if (y === 0) {
     throw new Error('divide by zero')
   }
 
   return x / y
+}
+
+export function ramdaDiv(x: number, y: number) {
+  return ramda.divide(x, y)
 }
