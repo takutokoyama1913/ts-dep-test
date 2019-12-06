8f4f44f86cc3a08c6316e2a7cb91808a4915530c
Takuto Koyama <koyama.takuto@kmw.jp>
2019-12-06 16:09:46 +0900

    test

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,14 @@
-import * as ramda from 'ramda';
-export function div(x, y) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ramda = require("ramda");
+function div(x, y) {
     if (y === 0) {
         throw new Error('divide by zero');
     }
     return x / y;
 }
-export function ramdaDiv(x, y) {
+exports.div = div;
+function ramdaDiv(x, y) {
     return ramda.divide(x, y);
 }
+exports.ramdaDiv = ramdaDiv;
